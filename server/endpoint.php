799fb30bae893f0c4edd73e2cbc762674bbaea18
paooolino <paooolino@gmail.com>
2016-07-16 16:37:58 +0200

    setup server responses

--- server/endpoint.php
+++ server/endpoint.php
@@ -1,8 +1,34 @@
 <?php
-    $resp = array(
-        "status" => "ko",
-        "serverErrorMessage" => "Impossibile accedere: credenziali errate."
-    );
+    // read raw post data
+    $json = file_get_contents('php://input');
+    $ARR = json_decode($json, true);  
+
+    echo json_encode(server($ARR));
     
-    echo json_encode($resp);
+function server($ARR) {
+    
+    switch( $ARR["action"] ) {
+        
+        case "login":
+            if( $ARR["user"] == "admin" && $ARR["pass"] == "admin" ) {
+                return array(
+                    "status" => "ok",
+                    "authCode" => "test_auth_code"
+                );
+            } else {
+                return array(
+                    "status" => "ko",
+                    "serverErrorMessage" => "Impossibile accedere: credenziali errate."
+                );
+            }
+            
+        default:
+            return array(
+                "status" => "ko",
+                "serverErrorMessage" => "Azione non valida."
+            );
+    }
+    
+}
+
     
